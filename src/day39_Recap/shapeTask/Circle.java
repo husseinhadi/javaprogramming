9c73c85e1c2730583165f4bbbf41791a9f34a0db
husseinhadi <hadi.hussein15@gmail.com>
2022-01-14 15:27:14 -0500

    day39: Recap

--- src/day39_Recap/shapeTask/Circle.java
+++ src/day39_Recap/shapeTask/Circle.java
@@ -22,21 +22,21 @@ public class Circle extends Shape{
         setRadius(radius);
     }
 
-    @Override
+
     public double area() {
         return radius* radius* pi;
     }
 
-    @Override
+
     public double perimeter() {
         return 2*radius*pi;
     }
 
-    @Override
+
     public String toString() {
         return "Circle{" +
                 "radius=" + radius +
-                "pi=" + pi +'\'' +
+                ", pi='" + pi + '\'' +
                 ", area='" + area() + '\'' +
                 ", perimeter='" + perimeter() + '\'' +
                 '}';
